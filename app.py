c8f799f461c99c5b592677ef25a9ea17b95ed24e
hrithik024 <87647003+hrithik024@users.noreply.github.com>
2023-08-27 16:07:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout
 import matplotlib.pyplot as plt
